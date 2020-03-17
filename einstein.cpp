81f46419dc43a5132ff656ff90f3dfa34af15ba4
macieryann <36053347+macieryann@users.noreply.github.com>
2020-03-17 08:57:24 -0400

    Update einstein.cpp

--- einstein.cpp
+++ einstein.cpp
@@ -25,52 +25,52 @@ int main() {			//start program
 		string orgNum = to_string(original);
 		
 		// check if first digit and third digit are not the same
-        if(orgNum[0] != orgNum[2]){
+        	if(orgNum[0] != orgNum[2]){
         
-    		// check if input number is between 100 and 999
-    		if (original < 1000 && original > 99) {
-    			
-    			// first reverse
-    			string temp = "";
-    			for(int i = orgNum.length() - 1; i >= 0; i--){
-    			    temp = temp.append(1, orgNum[i]);
-    			}
-    			cout << temp << endl;
-    			
-    			// change temp back to an INT for subtraction
-    			int myInt = stoi(temp);
-                
-    			// difference of original and reverse
-    			int difference = 0;
-    			if (original > myInt) {
-    				difference = original - myInt;
-    			} else {
-    				difference = myInt - original;
-    			}
-                cout << difference << endl;
-                
-                // reverse the difference
-                temp = "";
-                string revDiff = to_string(difference); // turn difference into a string for the for loop
-                
-                for(int j = orgNum.length() - 1; j >= 0; j--){
-    			    temp = temp.append(1, revDiff[j]);
-    			}
-    			
-    			cout << temp << endl;
-    		    
-    		    // back to integers
-    		    int finalInt = stoi(temp);
-    		    	
-    			// sum of difference and rev difference
-    			int finalAnswer;
-    			finalAnswer = finalInt + difference;
-    			cout << finalAnswer << endl;
-    
-    		} else {
-    			cout << "Number must be three digits.  Try again: " << endl;
-    			cin >> original;
-    		}
+			// check if input number is between 100 and 999
+			if (original < 1000 && original > 99) {
+
+				// first reverse
+				string temp = "";
+				for(int i = orgNum.length() - 1; i >= 0; i--){
+				    temp = temp.append(1, orgNum[i]);
+				}
+				cout << temp << endl;
+
+				// change temp back to an INT for subtraction
+				int myInt = stoi(temp);
+
+				// difference of original and reverse
+				int difference = 0;
+				if (original > myInt) {
+					difference = original - myInt;
+				} else {
+					difference = myInt - original;
+				}
+				cout << difference << endl;
+
+				// reverse the difference
+				temp = "";
+				string revDiff = to_string(difference); // turn difference into a string for the for loop
+
+				for(int j = orgNum.length() - 1; j >= 0; j--){
+					    temp = temp.append(1, revDiff[j]);
+				}
+
+				cout << temp << endl;
+
+				// back to integers
+				int finalInt = stoi(temp);
+
+				// sum of difference and rev difference
+				int finalAnswer;
+				finalAnswer = finalInt + difference;
+				cout << finalAnswer << endl;
+
+			} else {
+				cout << "Number must be three digits.  Try again: " << endl;
+				cin >> original;
+			}
     		
     		// ask user if they'd like to continue
     		cout << "Would you like to try another number (Y/N): ";
@@ -80,6 +80,6 @@ int main() {			//start program
     		}
         }
 
-	}
+}
 	return 0;
 }
